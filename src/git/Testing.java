d7dc86a7294b5d43aa099e83c2a28623b5e6efde
ainura <ainurkasymova12@gmail.com>
2022-02-16 22:32:22 -0500

    1st commit

---
+++ src/git/Testing.java
@@ -0,0 +1,11 @@
+package git;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(pw);
+System.out.println("water is blue");
+	}
+
+}
