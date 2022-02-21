ac2affd46957f2764354db2df44d294354dd61a1
ainura <ainurkasymova12@gmail.com>
2022-02-21 10:08:52 -0500

    2nd commit

--- src/git/Testing.java
+++ src/git/Testing.java
@@ -3,9 +3,19 @@ package git;
 public class Testing {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println(pw);
-System.out.println("water is blue");
+		// Create a 2D array of integer values. Print the sum of all numbers. 
+		
+				int [][] x = {
+						{10,21,40,15}
+						
+				};
+				int sum = 0; 
+				for (int i=0; i<x.length; i++) {
+					for (int j=0; j<x[i].length; j++) {
+						sum+=x[i][j];
+					}
+					System.out.println(sum);
+					sum=0;
 	}
 
-}
+	}}
